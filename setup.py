29839257ab51f5c805effa1eac4dd9454bbb72f1
Timofey Vilkov <tvvilkov@avito.ru>
2024-02-03 13:52:57 +0800

    add cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My diploma project',
+    author='Timofey Vilkov',
+    license='MIT',
+)
